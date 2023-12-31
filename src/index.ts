0728ecc85f2b71c2f3a0a719d64101bedd0e4822
Aanchal Adhikari <aadhikari37@gatech.edu>
2023-12-31 08:40:19 +0545

    Allow CORS

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from 'express';
 import dotenv from 'dotenv';
 import connectDb from './config/dbConnection';
+import cors from 'cors';
 
 import BatteryRoutes from './routes/batteryRoutes';
 import { errorHandler } from './middleware/errorHandler';
@@ -12,6 +13,8 @@ connectDb();
 const port = process.env.PORT || 5000;
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 
